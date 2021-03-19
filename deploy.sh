a805a19bbaea5f9b945bb2dc7a760c65ff9d6ae2
Giju Ahn <60786941+GijuAhn@users.noreply.github.com>
2021-03-19 19:55:09 +0900

    rebuilding site Fri Mar 19 19:55:09     2021

--- deploy.sh
+++ deploy.sh
@@ -34,3 +34,14 @@ fi
 git commit -m "$msg"
 
 git push origin +master
+
+# wait keypress
+echo "Press any key to continue"
+while [ true ] ; do
+read -t 3 -n 1
+if [ $? = 0 ] ; then
+exit ;
+else
+echo "waiting for the keypress"
+fi
+done
